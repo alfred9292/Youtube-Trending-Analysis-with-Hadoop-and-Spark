16267752e6993e6a60806172d09b86f7767c72ce
Alfie <39995471+alfred9292@users.noreply.github.com>
2021-06-29 15:48:18 +1000

    Update reducer.py

--- Hadoop Mapreduce/reducer.py
+++ Hadoop Mapreduce/reducer.py
@@ -6,7 +6,13 @@ dict_total = {}
 count1 = 0
 count2 = 0
 
-
+'''
+In the reducer phase, two dictionaries are created for each country respectively. 
+If the indicator shows 1, the record will be inserted to dictionary 1 with key value pair of video id and category. The same principle applies to indicators of 2. 
+The next step is to find from dictionary 1, how many records actually overlap with dictionary 2, the result will be stored in the dictionary same with key value pair of category and its corresponding counts. Then for country 1, 
+dictionary total is created to calculate the total number for each category in country 1. The last stage is to find for each category in the overlap dictionary, the corresponding count for dictionary total will be count 1 and the count for dictionary same will be count 2,
+ the answer will simply be count2/count1 and eventually print the output based on the required format.
+'''
 
 
 def reducer():
